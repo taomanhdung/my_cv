30d3e84c84e5366f8a24f3d20c01582e608e3c79
taomanhdung <tadusoft@gmail.com>
2017-01-01 22:31:30 +0700

    new update

--- _site/gulpfile.js
+++ _site/gulpfile.js
@@ -88,5 +88,6 @@ gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() 
     gulp.watch('js/*.js', ['minify-js']);
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('*.html', browserSync.reload);
+    //gulp.watch('_data/**', browserSync.reload);
     gulp.watch('js/**/*.js', browserSync.reload);
 });
